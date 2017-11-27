6beb61f22e23b8405c86e56ca98362e77a7018d9
diciembre-noche <jiajen.yang3@gmail.com>
2017-11-27 15:51:17 +0100

    search in a law name list to get the name of the law

--- search_string.py
+++ search_string.py
@@ -1,4 +1,21 @@
 # encoding=utf-8
+def look_up_law(content,lawstart,law_list):
+    for i in range(len(law_list)):
+        to_be_checked=content[lawstart-len(law_list[i])+3:lawstart+3]
+        if(to_be_checked==law_list[i]):
+            return(to_be_checked)
+    return "目錄裡沒有的法"
+f=open("lawlist.txt","r")
+content=f.read()
+f.close()
+law_list=content.split("\n")
+law_list.pop()
+
+x="一二三四五六七"
+y="一"
+print(y[0]==x[0])
+print(len(x))
+
 f=open("original_content.txt","r")
 content=f.read()
 f.close()
@@ -7,7 +24,8 @@ while(True):
     lawstart=content.find("法第",lawstart)
     if lawstart==-1:
         break
+    law_name=look_up_law(content,lawstart,law_list)
     article=content.find("條",lawstart)
-    print(content[lawstart-15:article+3])
+    print(law_name+content[lawstart+3:article+3])
     lawstart=article
     
