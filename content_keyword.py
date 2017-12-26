4226458ede53e194e35974439614f17435879b90
diciembre-noche <jiajen.yang3@gmail.com>
2017-12-26 11:00:18 +0100

    read in json, convert to txt and output csv

--- content_keyword.py
+++ content_keyword.py
@@ -2,14 +2,14 @@
 import json
 import jieba
 
-with open('test.json') as data_file:    
+with open('json/行政-104-訴更一-17.json') as data_file:    
     data = json.load(data_file,encoding="utf-8")
 main_content= json.dumps(data['verdict']['body']['content']['main_content'],ensure_ascii=False)    
 main_content=main_content.replace('\\r\\n','\n')
 main_content=main_content.encode('utf-8')
 print(main_content)
 
-f = open('original_content.txt', 'w')
+f = open('行政-104-訴更一-17.txt', 'w')
 f.write('%s' % main_content)  
 f.close()
 
